a19714e7bc864e68f60b93eb799a515ac0f0ff13
Glitch (fsd-backend) <none>
2023-03-15 12:36:28 +0000

    🍜📮 Checkpoint ./Routes/chat.js:55446265/930

--- Routes/chat.js
+++ Routes/chat.js
@@ -4,11 +4,26 @@ const conversationConstructor = module.require("../Schemas/conversation");
 
 
 router.get("/coversation/:uid",(req,res)=>{
-  conversationConstructor.find({})
+  let uid=req.params.uid
+  conversationConstructor.find({$or:[{user1:uid},{user2:uid}]})
+  .then((result)=>{
+    res.send(result);
+  })
+  .catch((err)=>{
+    res.send("error")
+  })
 });
 
 router.get("/message/:conversationId",(req,res)=>{
-  
+  let cid=req.params.conversationId
+  conversationConstructor.find({_id:cid})
+  .populate("messages")
+  .then((result)=>{
+    res.send(result);
+  })
+  .catch((err)=>{
+    res.send("error")
+  })
 });
 
 router.post("/message/add",(req,res)=>{
@@ -19,6 +34,19 @@ router.post("/message/add",(req,res)=>{
 
 router.post("/conversation/add",(req,res)=>{
   const data=req.body.data;
+  const uid1=data.user1;
+  const uid2=data.user2;
+  conversationConstructor.find({$or:
+                                [
+                                  {user1:uid1,user2:uid2},
+                                  {user1:uid2,user2:uid1}
+                                ]
+                               })
+    .then((result)=>{
+      if(result.length>0){
+        
+      }
+  })
 });
 
 module.exports=router;
