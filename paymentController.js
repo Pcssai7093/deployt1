07f375cc407ec8e5940218b87138611529625bf0
RISHIKBHIMAVARAPU <rishiktarak@gmail.com>
2023-04-25 21:49:19 +0530

    pc-3

--- paymentController.js
+++ paymentController.js
@@ -1,4 +1,5 @@
 const Razorpay = require("razorpay");
+var crypto = require("crypto");
 
 const KEY_ID = "rzp_test_BnefbrdGHpkF0K";
 const KEY_SECRET = "ziDCT4LbQQqMz19qGx3LfvRf";
@@ -22,9 +23,9 @@ module.exports.orders = (req, res) => {
 
 module.exports.verify = (req, res) => {
   let body =
-    req.body.razorpay_order_id +
+    req.body.response.razorpay_order_id +
     "|" +
-    req.body.razorpay_payment_id;
+    req.body.response.razorpay_payment_id;
 
   var expectedSignature = crypto
     .createHmac("sha256", KEY_SECRET)
@@ -32,7 +33,7 @@ module.exports.verify = (req, res) => {
     .digest("hex");
 
   var response = { signatureIsValid: "false" };
-  if (expectedSignature === req.body.razorpay_signature){
+  if (expectedSignature === req.body.response.razorpay_signature){
     res.send({ code: 200, message: "sign valid" });
   } else {
     res.send({ code: 500, message: "sign Invalid" });
